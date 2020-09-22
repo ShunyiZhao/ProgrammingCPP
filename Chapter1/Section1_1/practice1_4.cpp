eb368c6d439116f1d99a210540c445c5bbe203ed
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-09-22 11:40:27 +0800

    complete 1_1

---
+++ Chapter1/Section1_1/practice1_4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+    int nVal1 = 0, nVal2 = 0;
+
+    cout<<"Input two numbers:"<<endl;
+    cin>>nVal1>>nVal2;
+
+    cout<<nVal1<<" * "<<nVal2<<" = "<<nVal1 * nVal2 <<endl;
+
+    return 0;
+}
