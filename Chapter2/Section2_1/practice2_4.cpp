68adb7df9952e8c57124bc5dfd52542222c01f33
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-09-22 18:38:26 +0800

    add 2.4

---
+++ Chapter2/Section2_1/practice2_4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+    cout << 'a' << endl;
+    cout << L'a' << endl;
+    cout << "a" << endl;
+    cout << L"a" << endl;
+
+    return 0;
+}
