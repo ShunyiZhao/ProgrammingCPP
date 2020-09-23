1d9b06c826ad15e0c0c897bb0050e0bc019e99e1
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-09-23 22:08:21 +0800

    add 2.1

--- Chapter2/Section2_1/practice2_4.cpp
+++ Chapter2/Section2_1/practice2_4.cpp
@@ -4,10 +4,35 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-    cout << 'a' << endl;
-    cout << L'a' << endl;
-    cout << "a" << endl;
-    cout << L"a" << endl;
+    // block 1
+
+    cout << "\'a\':   " << 'a' << endl; // char a
+    cout << "L\'a\':   " << L'a' << endl; // long char a
+    cout << "\"a\":   " << "a" << endl;
+    cout << "L\"a\":   " << L"a" << endl;
+
+    // block 2
+
+    cout << "10:   " << 10 << endl;
+    cout << "10u:   " << 10u << endl;
+    cout << "10L:   " << 10L << endl;
+    cout << "10uL:   " << 10uL << endl;
+    cout << "012:   " << 012 << endl;
+    cout << "0xC:   " << 0xC << endl;
+
+    // block 3
+
+    cout << "3.14:   " << 3.14 << endl;
+    cout << "3.14f:   " << 3.14f << endl;
+    cout << "3.14L:   " << 3.14L << endl;
+
+    // block 4
+
+    cout << "10:   " << 10 << endl;
+    cout << "10u:   " << 10u << endl;
+    cout << "10.:   " << 10. << endl;
+    cout << "10e-2:   " << 10e-2 << endl;
+
 
     return 0;
 }
---
+++ Chapter2/Section2_1/practice2_5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+    int month1 = 9, day1 = 7;
+    int month2 = 07, day2 = 07;
+
+    cout << "month1:   " << month1 << endl << "day1:   " << day1 << endl;
+    cout << "month2:   " << month2 << endl << "day2:   " << day2 << endl;
+
+    return 0;
+}
