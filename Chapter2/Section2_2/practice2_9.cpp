e36f03caaea4f6e3d83673f13af04f0a654d122d
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-09-28 20:38:37 +0800

    add 2_11 2_9

---
+++ Chapter2/Section2_2/practice2_9.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int main(int argc, char **argv){
+
+    /*
+    std::cin >> int input_value;
+    */
+
+    int input_value;
+    //std::cin >> input_value;
+    //std::cout << input_value << std::endl;
+
+    /*
+    int i = { 3.14 };
+    */
+    int i = 3.14;
+
+    /*
+    double salary = wage = 9999.99;
+    */
+    double salary = 9999.99, wage = 9999.99;
+
+    /*
+    */
+    int i2 = 3.14;
+
+    return 0;
+}
