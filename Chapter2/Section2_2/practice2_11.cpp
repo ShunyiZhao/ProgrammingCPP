adcca39387acdc890a60ad209b8442c9e25fc3d7
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-09-28 21:01:04 +0800

    add 2.12 in 2.11

--- Chapter2/Section2_2/practice2_11.cpp
+++ Chapter2/Section2_2/practice2_11.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 
 int main(int argc, char **argv){
-    
+
+    //extern int ix = 1024; // declaration and definition, but clang++ not allow this method
+    int iy; // declaration and definition
+    extern int iz; //declaration
+
+    double Double = 3.14;
+
     return 0;
 }
